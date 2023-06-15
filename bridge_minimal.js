53fc6b86bc83dd0875e08a83ec8896705a764739
OB7TEAM <122007778+OB7TEAM@users.noreply.github.com>
2023-06-15 10:25:10 +0800

    Update bridge_minimal.js

--- bridge_minimal.js
+++ bridge_minimal.js
@@ -1,15 +1,16 @@
 function init()
 {
-	vkBridge.send('VKWebAppInit').catch((error) => {console.log(error);});
+	//vkBridge.send('VKWebAppInit').catch((error) => {console.log(error);});
 }
 
 function joingroup()
 {
-	vkBridge.send('VKWebAppJoinGroup', {group_id: 218386519}).catch((error) => {console.log(error);});
+	vkBridge.send('VKWebAppJoinGroup', {group_id: '218386519'}).catch((error) => {console.log(error);});
 }
 
 function recommend()
 {
-	vkBridge.send('VKWebAppShowInviteBox');
+	//vkBridge.send('VKWebAppShowInviteBox');
+	vkBridge.send('VKWebAppJoinGroup', {group_id: '218386519'}).catch((error) => {console.log(error);});
 }
 
